6fb79c807a24d18f6039a1151a08ea9a656caf43
Bobbi Yogatama <bwyogatama@gmail.com>
2025-11-10 19:21:17 +0000

    Cleanup dead code

--- src/include/operator/gpu_physical_projection.hpp
+++ src/include/operator/gpu_physical_projection.hpp
@@ -17,7 +17,6 @@
 #pragma once
 
 #include "gpu_physical_operator.hpp"
-// #include "duckdb/execution/physical_operator.hpp"
 #include "duckdb/planner/expression.hpp"
 
 namespace duckdb {
@@ -32,29 +31,6 @@ public:
 
 	vector<unique_ptr<Expression>> select_list;
 
-	// OperatorResultType Execute(ExecutionContext &context, GPUIntermediateRelation &input_relation, GPUIntermediateRelation &output_relation,
-	// 									GlobalOperatorState &gstate, OperatorState &state) const override;
-
 	OperatorResultType Execute(GPUIntermediateRelation &input_relation, GPUIntermediateRelation &output_relation) const override;
-
-	// GPUExpressionExecutor* gpu_expression_executor;
-
-public:
-	// unique_ptr<OperatorState> GetOperatorState(ExecutionContext &context) const override;
-	// OperatorResultType Execute(ExecutionContext &context, DataChunk &input, DataChunk &chunk,
-	//                            GlobalOperatorState &gstate, OperatorState &state) const override;
-
-	// bool ParallelOperator() const override {
-	// 	return true;
-	// }
-
-	// string ParamsToString() const override;
-
-	// static unique_ptr<PhysicalOperator>
-	// CreateJoinProjection(vector<LogicalType> proj_types, const vector<LogicalType> &lhs_types,
-	//                      const vector<LogicalType> &rhs_types, const vector<idx_t> &left_projection_map,
-	//                      const vector<idx_t> &right_projection_map, const idx_t estimated_cardinality);
-
-
 };
 } // namespace duckdb
